de9bfb14c0d02cc15c8d02f7022de3cf0364d474
Kwesil <nwabikejr@gmail.com>
2025-08-21 11:47:56 +0100

    Working with strings

---
+++ c++Class/strings.cpp
@@ -0,0 +1,32 @@
+// working with strings in C++
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a = "hello";
+    string b = "world!";
+    string c = a + " " + b; // concatenation
+    string d;
+    int e;
+    
+    cout << c <<endl;
+    
+    cout << "What is your name? " << endl;
+    cin >> d; // input from user
+
+    cout << "How old are you" << endl;
+    cin >> e; // input from user
+
+    cout << "You are welcome " + d + "!" << endl;
+    
+    if(e > 18){
+        cout << "You are eligible to use this platform!" << endl;
+    } else{
+        cout << "You are not eligible to use this platform" << endl;
+    };
+
+
+    
+}
